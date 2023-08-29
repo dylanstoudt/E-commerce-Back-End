e01f8b49370d37933b648fb307157044c17fb63b
dylanstoudt <138310076+dylanstoudt@users.noreply.github.com>
2023-08-29 17:53:18 -0400

    Created columns for Product model and Category model, Began linking models using foreign keys

--- models/index.js
+++ models/index.js
@@ -5,8 +5,15 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: '',
+});
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'driver_id',
+  onDelete: 'CASCADE',
+});
 
 // Products belongToMany Tags (through ProductTag)
 
