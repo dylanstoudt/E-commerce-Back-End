3d07156001a96dccb934019addc7fdf5bc05c56a
dylanstoudt <138310076+dylanstoudt@users.noreply.github.com>
2023-08-30 14:44:58 -0400

    fixed foreign keys in index.js to remove duplicate ids showing

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -5,24 +5,67 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 router.get('/', (req, res) => {
   // find all tags
-  // be sure to include its associated Product data
+  Tag.findAll({
+    // include its associated Product data
+    include: [{ model: Product }]
+  }).then((tagData) => {
+    res.json(tagData)
+  })
 });
 
 router.get('/:id', (req, res) => {
   // find a single tag by its `id`
-  // be sure to include its associated Product data
+  Tag.findByPk(req.params.id, {
+    // include its associated Product data
+    include: [{ model: Product}]
+  })
+    .then((tagData) => {
+    res.json(tagData)
+  });
 });
 
 router.post('/', (req, res) => {
   // create a new tag
+  Tag.create(req.body)
+  .then((newTag) => {
+    res.json(newTag);
+  })
+  .catch((err) => {
+    res.json(err)
+  })
 });
 
 router.put('/:id', (req, res) => {
   // update a tag's name by its `id` value
+  Tag.update(
+    {
+      tag_name: req.body.tag_name
+    },
+    {
+      // check params to verify which tag to updates
+      where: {
+        id: req.params.id
+      }
+    }
+  )
+    .then((updatedTag) => {
+      res.json(updatedTag)
+    })
+    .catch((err) => res.json(err))
 });
 
 router.delete('/:id', (req, res) => {
-  // delete on tag by its `id` value
+  // delete tag based off ID
+  Tag.destroy({
+      // check params to verify which tag to updates
+      where:{
+      id: req.params.id
+    }
+  })
+  .then((deletedTag) => {
+    res.json(deletedTag);
+  })
+  .catch((err) => res.json(err))
 });
 
 module.exports = router;
